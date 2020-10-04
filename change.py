6e75b9574f134fdfd73af57c7ab2705b38e14750
acctwdxab <71532324+acctwdxab@users.noreply.github.com>
2020-10-04 16:31:37 -0700

    Add files via upload

--- change.py
+++ change.py
@@ -5,7 +5,7 @@ resultQ=num//25
 resultD=(num%25)//10
 resultN=(num%25)%10//5
 resultP=((num%25)%10)%5
-print("Q: ", resultQ)
-print("D: ", resultD)
-print("N: ", resultN)
-print("P: ", resultP)
+print("Q:", resultQ)
+print("D:", resultD)
+print("N:", resultN)
+print("P:", resultP)
